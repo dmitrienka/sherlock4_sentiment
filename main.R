fc0e796e4379dd89531f774e00b07245eadaabc3
dmitrienka <dmitrienka@gmail.com>
2017-01-02 15:52:56 +0300

    initial

---
+++ main.R
@@ -0,0 +1,11 @@
+library(twitteR)
+library(tm)
+library(SnowballC)
+library(MASS)
+library(wordcloud)
+
+# setup with the twitter app keys:
+#setup_twitter_oauth(consumer_key, consumer_secret, token, token_secret)
+source("./setup.R")
+
+# search
